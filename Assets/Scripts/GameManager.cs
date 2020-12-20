a35b7f3b26f0856097c6eb05872ce72f243cfe07
ZitaRR <Haeggqvist1998@hotmail.com>
2020-12-20 14:34:06 +0100

    Added scene loading with loading screen

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,10 +1,15 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; }
     public Environment Environment { get; private set; }
-    public PlayerEntity Player { get; private set; }
+    public PlayerEntity Player => player.GetComponent<PlayerEntity>();
+
+    [SerializeField]
+    private GameObject player;
 
     private void Awake()
     {
@@ -14,7 +19,18 @@ public class GameManager : MonoBehaviour
         Instance = this;
         DontDestroyOnLoad(gameObject);
 
-        Environment = GetComponent<Environment>();
-        Player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerEntity>();
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    public void LoadScene(string name)
+        => SceneLoader.LoadScene(name);
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (!scene.name.Contains("PL") && scene.name != "SampleScene")
+            return;
+
+        Environment = gameObject.AddComponent<Environment>();
+        player = Instantiate(player, new Vector2(8, 8), Quaternion.identity);
     }
 }
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public sealed class SceneLoader : MonoBehaviour
+{
+    public static string SceneName { get; private set; }
+
+    private AsyncOperation loading;
+    private CanvasGroup group;
+
+    [SerializeField]
+    private Text loadingText;
+    [SerializeField]
+    private Slider loadingBar;
+
+    private void Awake()
+    {
+        group = GetComponent<CanvasGroup>();
+    }
+
+    private void Start()
+    {
+        StartCoroutine(Load());
+    }
+
+    private IEnumerator Load()
+    {
+        yield return StartCoroutine(SetAlpha(1, 1));
+        loading = SceneManager.LoadSceneAsync(SceneName);
+        while (!loading.isDone)
+        {
+            loadingText.text = $"Loading {loading.progress * 100:00}%";
+            loadingBar.value = loading.progress * 100;
+            yield return null;
+        }
+        yield return StartCoroutine(SetAlpha(0, 1));
+    }
+
+    private IEnumerator SetAlpha(float alpha, float duration)
+    {
+        float start = group.alpha;
+        float time = 0;
+
+        while (time < duration)
+        {
+            group.alpha = Mathf.Lerp(start, alpha, time / duration);
+            time += Time.deltaTime;
+            yield return null;
+        }
+        group.alpha = alpha;
+    }
+
+    public static void LoadScene(string name)
+    {
+        SceneName = name;
+        SceneManager.LoadScene("LoadingScene");
+    }
+}
