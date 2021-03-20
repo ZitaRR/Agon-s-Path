dbc200e464975acd178f699ab98a78a689a70bd3
ZitaRR <Haeggqvist1998@hotmail.com>
2021-03-20 22:04:10 +0100

    Sprite offset and changes to animations
    Created an sprite offset for the goblin due to it not being centered and
    restructered entity prefabs.
    Some minor changes/additions to animations due to the entity prefab
    restructure.

---
+++ Assets/Scripts/EntityAnimationHelper.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntityAnimationHelper : MonoBehaviour
+{
+    private Entity entity;
+
+    private void Awake()
+    {
+        entity = GetComponentInParent<Entity>();
+    }
+
+    private void RunAnimation()
+    {
+        entity.Attack();
+    }
+}
