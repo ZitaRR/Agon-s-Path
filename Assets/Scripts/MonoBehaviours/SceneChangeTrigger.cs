1e37d7cc8bccc7bada75382a359c9f592e86bf22
ZitaRR <Haeggqvist1998@hotmail.com>
2021-03-22 12:25:28 +0100

    Scene transition triggers and minor stuff
    Added scene transition triggers and made changes to prefabs and scenes.
    Also added materials for scene transition trigger particles.

---
+++ Assets/Scripts/MonoBehaviours/SceneChangeTrigger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class SceneChangeTrigger : MonoBehaviour
+{
+    [SerializeField]
+    private string sceneName;
+    [SerializeField]
+    private Vector2 playerSpawnPosition;
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        Entity entity = collider.GetComponent<Entity>();
+        if (!(entity is PlayerEntity))
+            return;
+
+        GameManager.Instance.LoadScene(sceneName, playerSpawnPosition);
+    }
+}
